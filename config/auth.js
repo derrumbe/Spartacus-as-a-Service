f2f18959d5f32c6c209b74265b4431d0afd683c8
derrumbe <github@irisar.com>
2019-02-25 20:58:57 -0600

    Removing old keys

--- config/auth.js
+++ config/auth.js
@@ -4,22 +4,22 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'      : '601139403643096', // your App ID
-        'clientSecret'  : '91e614b3523e558e5e9b219f75f0ca15', // your App Secret
+        'clientID'      : '', // your App ID
+        'clientSecret'  : '', // your App Secret
         'callbackURL'   : 'http://localhost:3000/auth/facebook/callback',
         'profileURL'    : 'https://graph.facebook.com/v2.8/me?fields=first_name,last_name,email',
         'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
     },
 
     'twitterAuth' : {
-        'consumerKey'       : 'FZ0gPKDIGFvcGY1SNgRfNGdzE',
-        'consumerSecret'    : 'JaslAhQiGCEafd3PXycFurTD4bMbC3U28xUq9RMDZkiRWJIu0C',
+        'consumerKey'       : '',
+        'consumerSecret'    : '',
         'callbackURL'       : 'http://localhost:3000/auth/twitter/callback'
     },
 
     'googleAuth' : {
-        'clientID'      : '103876422101-g1b6h3kg3scc9k1q3uhfgm3fpld54je2.apps.googleusercontent.com',
-        'clientSecret'  : 'vghjS_0jkv-vwx_hCKk1tOiM',
+        'clientID'      : '',
+        'clientSecret'  : '',
         'callbackURL'   : 'http://localhost:3000/auth/google/callback'
     }
 
